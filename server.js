71f118c45b103367f418e5538678db12cbf80eb1
hideto.fukuda <hideto.fukuda@uzabase.com>
2023-09-29 03:01:24 +0900

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
       isBase64Encoded : boolean,
-      statusCode: 200,
+      statusCode: "200",
       headers: { "Content-Type":"text/plain" },
       body: JSON.stringify('Hello from Lambda!'),
   };
