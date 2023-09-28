0ada85fe1f1cdd76395ca340c361d87634bfd6ac
hideto.fukuda <hideto.fukuda@uzabase.com>
2023-09-29 02:48:39 +0900

    test

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+exports.handler = async (event) => {
+  const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
