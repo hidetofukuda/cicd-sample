4065a7bd2d392e2047f8712a8b04a4a2be96927e
hideto.fukuda <hideto.fukuda@uzabase.com>
2023-09-29 17:33:38 +0900

    test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM public.ecr.aws/docker/library/node:18
+FROM public.ecr.aws/lambda/nodejs:18
 
 # Copy function code
 # COPY index.js ${LAMBDA_TASK_ROOT}
-COPY . /
+COPY index.js package.json package-lock.json /var/task
+RUN npm install
 
 # Set the CMD to your handler (could also be done as a parameter override outside of the Dockerfile)
-CMD [ "npm", "start" ]
+CMD ["index.handler"]
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// exports.handler = async (event) => {
-//   const response = {
-//     statusCode: 200,
-//     "headers":{ ContentType:"text/html"},
-//     body: JSON.stringify('Hello from Lambda!'),
-//   };
-//   return response;
-// };
+exports.handler = async () => {
+  const response = {
+    statusCode: 200,
+    "headers":{ ContentType:"text/html"},
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
 
-console.log("Hello from Lambda!")
+console.log("test")
