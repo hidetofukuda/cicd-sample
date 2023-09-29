438c4913057a99d1b4c8b46f32bec26875496c1e
hideto.fukuda <hideto.fukuda@uzabase.com>
2023-09-29 18:50:44 +0900

    change string

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async () => {
   const response = {
     statusCode: 200,
     "headers":{ ContentType:"text/html"},
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify('Hello from Lambdaaaaaaa!'),
   };
   return response;
 };
